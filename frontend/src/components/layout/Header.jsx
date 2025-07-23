5d13cf78d0439c5cc8eaf0e6e32469bf667380a2
lee0su <leeys4903@naver.com>
2025-07-23 18:15:20 +0900

    feat: CSS 파일 추가

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../../pages/Main/MainPage.css";
+import "./Header.css";
 
 function Header() {
+  // 임시 로그인 상태 (실제 서비스에서는 context, redux, props 등으로 대체)
+  const isLoggedIn = false;
+
   return (
     <header className="header">
       <div className="header-inner">
@@ -14,8 +18,17 @@ function Header() {
           <a href="#">분석</a>
         </nav>
         <div className="auth">
-          <Link to="/login" className="login">로그인</Link>
-          <Link to="/signup" className="signup">회원가입</Link>
+          {isLoggedIn ? (
+            <div className="header-profile">
+              <div className="header-profile-img">N</div>
+              <span className="header-profile-nickname">닉네임</span>
+            </div>
+          ) : (
+            <>
+              <Link to="/login" className="login">로그인</Link>
+              <Link to="/signup" className="signup">회원가입</Link>
+            </>
+          )}
         </div>
       </div>
     </header>
