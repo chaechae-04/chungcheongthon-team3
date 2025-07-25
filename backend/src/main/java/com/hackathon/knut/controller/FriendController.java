e0e83d0832a7d608b5da850c527b9d27458fe8da
i3inni <fkdl4862@a.ut.ac.kr>
2025-07-25 19:33:47 +0900

    1차 회원가입

--- backend/src/main/java/com/hackathon/knut/controller/FriendController.java
+++ backend/src/main/java/com/hackathon/knut/controller/FriendController.java
@@ -8,13 +8,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller // 컨트롤러 선언
+@RestController
 @RequiredArgsConstructor // 생성자를 자동으로 만들어줌
 @RequestMapping("/api/friends") // 기본적인 요청 경로
 @CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173"}) // 필요하면 CORS 설정
 public class FriendController {
 
     private final FriendService friendService; // FriendService 객체 생성
+
+    // 임시
+    @GetMapping
+    public ResponseEntity<String> getFriendApiStatus() {
+        return ResponseEntity.ok("Friends API ready!");
+    }
 
     @PostMapping
     public ResponseEntity<String> addFriend(@RequestBody FriendAddRequest request) { // 클라이언트의 String 요청을 FriendAddRequest 객체로 변환해 파라미터로 받음
